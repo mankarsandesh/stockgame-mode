78b721ddd205f2f86e43a4fe519731e9c76fad48
Sandesh <mankarsandesh11@gmail.com>
2020-08-04 17:10:59 +0700

    working on game lobby

--- src/components/GameLobby/GameLobby.js
+++ src/components/GameLobby/GameLobby.js
@@ -1,16 +1,44 @@
 import React from 'react'
 import "./GameLobby.css";
+import MoneyIcon from '@material-ui/icons/Money';
+
 function GameLobby() {
     return (
         <div className="gameLobby__wrapper">
             
             <div className="gameLobby__StockInfo">
-                <h2>Stock info</h2>
+
+                <div className="stockInfo__game">
+                    <span>Game Id : </span> BDJBJBDJ
+                </div>
+                <div className="stockInfo__game">
+                    <span>BET Id : </span> BDJBJBDJ
+                </div>
+                <div className="stockInfo__game">
+                    <span>BET : </span> FIG_BIG
+                </div>
+                <div className="stockInfo__game">
+                    <span>Stock Name : </span> BIT12               
+                </div>
+                <div className="stockInfo__gameButton">
+                    <button> <MoneyIcon /> &nbsp;View ALL</button>              
+                </div>
+
+
             </div>
             <div className="gameLobby__Betting">
 
                 <div className="gameLobby__SelectStock"> 
-                <h2>Select info</h2>
+                    <div className="gameLobby__lastDraw">
+                        <h2>Last Draw</h2>
+                        <h3>2868.48</h3>
+                    </div>
+                    <h2 className="gameLobby__stockName">SELECT sTOCK</h2>
+                    <div className="gameLobby__StockSelect">
+                    <button>Select Stock</button> 
+                    <button>BTC/USDT</button> 
+                    <button> 1 MInute</button> 
+                    </div>
                 </div>
                 <div className="gameLobby__GameRule"> 
                 <h2>Game Rule</h2>
