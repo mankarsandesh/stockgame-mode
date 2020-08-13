b405b3339f34fe52e9ac004984173f6d0de27f21
Sandesh <mankarsandesh11@gmail.com>
2020-08-13 13:46:30 +0700

    Leaderboard Design Finsh

--- src/components/pages/Leaderboard/Leaderboard.js
+++ src/components/pages/Leaderboard/Leaderboard.js
@@ -6,26 +6,46 @@ import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import { Button } from "@material-ui/core";
 import EventIcon from "@material-ui/icons/Event";
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    '@global': {
+      '*::-webkit-scrollbar': {
+        width: '0.6em'
+      },
+      '*::-webkit-scrollbar-track': {
+        '-webkit-box-shadow': 'inset 10px 0 6px #e7be4e'       
+      },
+      '*::-webkit-scrollbar-thumb': {
+        backgroundColor: '#a72b2b ',
+        outline: '1px solid #e7be4e',
+        borderRadius :'10px'
+      }
+    },
+    root : {
+      backgroundColor: '#191818',
+      padding:'0px 15px 15px 15px',
+    },
     paper: {
-      backgroundColor: "#2e2e2e",
+      backgroundColor: "#191818",
       overflowY: "auto",
-      height: "600px",
+      height: "500px",
+      padding:'0',
     },
     userImage: {
       width: 60,
       height: 60,
       display: "flex",
       alignItems: "center",
-      margin: "10px 0px",
+      marginTop: "5px",
     },
     img: {
+      border:'3px solid white',
       borderRadius: "50%",
       margin: "auto",
       display: "block",
-      maxWidth: "60PX",
-      maxHeight: "60PX",
+      maxWidth: "50PX",
+      maxHeight: "50PX",
     },
     textRate: {
       fontSize: "12px",
@@ -37,7 +57,7 @@ const useStyles = makeStyles((theme: Theme) =>
     bodyRate: {
       fontSize: "18px",
       fontWeight: "800",
-      color: "#2EFF4D ",
+      color: "#0fa21f",
       textAlign: "center",
     },
     textBets: {
@@ -50,7 +70,7 @@ const useStyles = makeStyles((theme: Theme) =>
     bodyBets: {
       fontSize: "18px",
       fontWeight: "800",
-      color: "#FF3F6B",
+      color: "#d30e63",
       textAlign: "center",
     },
     textAmount: {
@@ -63,19 +83,19 @@ const useStyles = makeStyles((theme: Theme) =>
     bodyAmount: {
       fontSize: "18px",
       fontWeight: "800",
-      color: "#FFDE25",
+      color: "#e7be4e",
       textAlign: "center",
     },
     followButton: {
       textAlign: "center",
       borderRadius: "20px",
       padding: "4px 14px",
-      border: "3px solid #FFDE25",
+      border: "3px solid #e7be4e",
       fontSize: "12px",
       fontWeight: "600",
       textTransform: "uppercase",
-      color: "white",
-      backgroundColor: "#9B281A",
+      color: "#e7be4e",
+      backgroundColor: "#a72b2b",
       display: "flex",
       alignItems: "center",
       margin: "15px auto",
@@ -94,6 +114,7 @@ const useStyles = makeStyles((theme: Theme) =>
     sortingWrapper: {
       display: "flex",
       color: "white",
+      backgroundColor: "#191818",
     },
     sortingLeft: {
       alignItems: "flex-start",
@@ -110,26 +131,55 @@ const useStyles = makeStyles((theme: Theme) =>
       color: "white",
       fontSize: "12px",
       marginRight: "6px",
+      fontWeight:'600',
+      padding:'0',
     },
+    ratingButtonActive :{
+      color: "#e7be4e",
+      fontSize: "12px",
+      marginRight: "6px",
+      fontWeight:'600',
+      padding:'0',
+    },
+    h2 : {
+      color : '#cfa640;',
+      fontWeight : '800',
+      textTransform : 'uppercase',
+      textAlign:'center',
+      backgroundColor : '#191818',
+      margin:'20px 0px 0px 20px'
+    },
+    sortingText : {
+      fontSize :'12px',
+      fontWeight : '400',
+      color:'grey',
+      textTransform:'capitalize '
+    }
   })
 );
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export default function ComplexGrid() {
   const classes = useStyles();
 
+  const [ sortButton , setSortButton ]  = React.useState('weekly');
+
+  const sortingRanking = (value) => {
+    setSortButton(value);
+  };
+
   return (
-    <div className={classes.root} className="header__AllPageTabPanel">
-      <h2>Leaderboard</h2>
+    <div className={classes.root} >
+      <h2 className={classes.h2}>Leaderboard</h2>
       <Grid className={classes.sortingWrapper} container>
         <Grid item xs={8} className={classes.sortingLeft}>
-          <h4>Top 10 Leaders</h4>
+          <h4>Top 10 Leaders <small className={classes.sortingText}>(Sorting by {sortButton})</small></h4> 
         </Grid>
         <Grid item xs={4} className={classes.sortingLeaders}>
-          <Button className={classes.ratingButton}>
+          <Button onClick={() => sortingRanking('weekly')} className={ sortButton == 'weekly' ? classes.ratingButtonActive : classes.ratingButton}>
             {" "}
-            <EventIcon /> Weekly Ranking
+            <EventIcon /> Weekly Ranking 
           </Button>
-          <Button className={classes.ratingButton}>
+          <Button onClick={() => sortingRanking('monthly')}  className={sortButton != 'weekly' ? classes.ratingButtonActive : classes.ratingButton}>
             {" "}
             <EventIcon /> Monthly Ranking
           </Button>
@@ -145,7 +195,7 @@ export default function ComplexGrid() {
                   alt="Sandesh Mankar"
                   src="https://miro.medium.com/fit/c/160/160/2*fQO45CfgZstbBjeHxeSxHA.jpeg"
                 />
-                <h4 className={classes.userName}>user12{number} </h4>
+                <h4 className={classes.userName}>User123444{number} </h4>
               </div>
             </Grid>
             <Grid item xs={3}>
