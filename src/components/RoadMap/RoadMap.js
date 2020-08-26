4cb75f20c96688eff910ba49e9173ebe389ee04d
Sandesh <mankarsandesh11@gmail.com>
2020-08-26 15:21:38 +0700

    Road map design issue done

--- src/components/RoadMap/RoadMap.js
+++ src/components/RoadMap/RoadMap.js
@@ -1,71 +1,50 @@
 import React from "react";
 import TableTrendMap from "components/TrandMap/TableTrendMap";
-import { createStyles, makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    roadMapWrapper: {
-      padding: "50px 40px",
-      textAlign: "center",
-    },
-    firstDigit: {
-      textAlign: "center",
-      textTransform: "uppercase",
-      border: "1px solid #e7bb27",
-      padding: "6px 14px",
-      color: " #e7bb27",
-      fontWeight: "600",
-      cursor: "pointer",
-      backgroundColor: "#303b77",
-      borderTopLeftRadius: "4px",
-    },
-    lastDigit: {
-      textAlign: "center",
-      textTransform: "uppercase",
-      border: "1px solid #e7bb27",
-      padding: "6px 14px",
-      color: " #e7bb27",
-      fontWeight: "600",
-      cursor: "pointer",
-      backgroundColor: "#653e21",
-    },
-    bothDigit: {
-      textAlign: "center",
-      textTransform: "uppercase",
-      border: "1px solid #e7bb27",
-      padding: "6px 14px",
-      color: " #e7bb27",
-      fontWeight: "600",
-      cursor: "pointer",
-      backgroundColor: "#2f5f2a",
-    },
-    twoDigit: {
-      textAlign: "center",
-      textTransform: "uppercase",
-      border: "1px solid #e7bb27",
-      padding: "6px 14px",
-      color: " #e7bb27",
-      fontWeight: "600",
-      cursor: "pointer",
-      backgroundColor: "#a72a2c",
-      borderTopRightRadius: "4px",
-    },
-  })
-);
+import "./RoadMap.css";
 
 const numbers = [1, 2, 3, 4];
+
 export default function RoadMap() {
-  const classes = useStyles();
+  const [gameRule, setGameRule] = React.useState("firstDigit");
+
+  const handleOpen = (pageName) => {
+    setGameRule(pageName);
+    console.log(pageName, "pageName");
+  };
+
+  const getClass = (someInput) => {
+    switch (someInput) {
+      case "firstDigit": {
+        return "button firstDigit";
+      }
+      case "lastDigit": {
+        return "button lastDigit";
+      }
+      case "twoDigit": {
+        return "button twoDigit";
+      }
+      case "bothDigit": {
+        return "button bothDigit";
+      }
+    }
+  };
+
+  const allRuleName = ["firstDigit", "lastDigit", "twoDigit", "bothDigit"];
+
   return (
-    <div className={classes.roadMapWrapper}>
-      <button className={classes.firstDigit}>First Digit </button>
-      <button className={classes.lastDigit}>last Digit </button>
-      <button className={classes.bothDigit}>Both Digit </button>
-      <button className={classes.twoDigit}>TWO Digit </button>
+    <div className="roadMapWrapper">
+      {allRuleName.map((ruleName) => (
+        <span
+          className={getClass(ruleName)}
+          onClick={() => handleOpen(ruleName)}
+        >
+          {ruleName}
+        </span>
+      ))}
 
       <div className="roadMap__data">
         {numbers.map((number) => (
-          <TableTrendMap key={number} />
+          <TableTrendMap key={number} gameRule={gameRule} />
         ))}
       </div>
     </div>
--- src/components/TrandMap/TableTrendMap.js
+++ src/components/TrandMap/TableTrendMap.js
@@ -1,31 +1,47 @@
-import React from 'react'
+import React from "react";
 import "./TableTrend.css";
 
 function createTable() {
-    let table = []
-    // Outer loop to create parent
-    for (let i = 0; i < 6; i++) {
-        let children = []
-        //Inner loop to create children
-        for (let j = 0; j < 18; j++) {
-            children.push(<td className={`${ j > 5 ? 'bigDigit' : 'firstDigit'}`} key={j}  >B</td>)
-        }
-        //Create the parent and add the children
-        table.push(<tbody><tr className="section"  key={children} >{children}</tr></tbody>)
+  let table = [];
+  // Outer loop to create parent
+  for (let i = 0; i < 6; i++) {
+    let children = [];
+    //Inner loop to create children
+    for (let j = 0; j < 18; j++) {
+      children.push(
+        <td className={`${j > 5 ? "bigDigit" : "firstDigit"}`} key={j}>
+          B
+        </td>
+      );
     }
-    return table
+    //Create the parent and add the children
+    table.push(
+      <tbody>
+        <tr className="section" key={children}>
+          {children}
+        </tr>
+      </tbody>
+    );
+  }
+  return table;
 }
 
-function TableTrendMap() {
+class TableTrendMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.gameRule,
+    };
+  }
+  render() {
     return (
-        <div className="tableTrend__wrapper">
-            <div className="tableTrend__firstDigit">
-                <table>
-                    {createTable()}
-                </table>
-            </div>            
+      <div className="tableTrend__wrapper">
+        <div className="tableTrend__firstDigit">
+          <table>{createTable()}</table>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default TableTrendMap
+export default TableTrendMap;
