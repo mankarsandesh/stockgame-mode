e461db83eb204f6fe83c400752e880a2994cb00a
Sandesh <mankarsandesh11@gmail.com>
2020-08-28 16:04:44 +0700

    Loader Page add on Main Page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React from "react";
 import "./App.css";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useParams } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Test from './components/Test/Test';
+import Test from "./components/Test/Test";
+import { render } from "react-dom";
 
+class App extends React.Component {
+  // constructor() {
+  //   this.state = { isLoading: true };
+  // }
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
+  componentDidMount(){
 
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/test" component={Test}></Route>
-        </Switch>
+   
+    console.log("App Mounted");
+  }
 
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/test" component={Test}></Route>
+          </Switch>
+
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
